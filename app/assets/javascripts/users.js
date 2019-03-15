3e8902b9e275818c3eaf5dea5b3a6bcb27ec90bd
carolgreene <carolgreene3@gmail.com>
2019-03-15 14:03:25 -0700

    cleaned up spacing on users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -10,10 +10,10 @@ function bindEventListeners() {
 }
 
 function listenForClickSignInForm() {
- $('.sign_in').on('click', (e) => {
+  $('.sign_in').on('click', (e) => {
     e.preventDefault() 
     getSignInForm() 
- })
+  })
 }
 
 function getSignInForm() {
@@ -26,7 +26,6 @@ function getSignInForm() {
     $(`#app-container`).html('').append(response)
   })    
 }
-
 
 
 function listenForClickNewUserForm() { 
@@ -48,7 +47,6 @@ function getNewUserForm() {
 }
 
 
-
 function postSignIn() {
   $(document).on('submit', "form#signin_form", function(e) {
     e.preventDefault()   
@@ -60,19 +58,18 @@ function postSignIn() {
        dataType: 'json',
        success: function(response)
        {
-        let newUser = new User(response)               
-        let userHtml = newUser.formatShow()
-        let userLinks = newUser.formatLinks()
-        $(`#heading`).html('')
-        $(`#app-container`).html('').append(userHtml) 
-        $(`#nav`).append(userLinks)
+         let newUser = new User(response)               
+         let userHtml = newUser.formatShow()
+         let userLinks = newUser.formatLinks()
+
+         $(`#heading`).html('')
+         $(`#app-container`).html('').append(userHtml) 
+         $(`#nav`).append(userLinks)
        }
     })
   })
 }      
-            
-            
-            
+                    
 
 function postNewUser() {
   $(document).on('submit', "form#new_user.new_user", function(e) {
@@ -99,34 +96,33 @@ function postNewUser() {
 
 
 class User {
-    constructor(user) {
-        this.id = user.id 
-        this.name = user.name 
-        this.role = user.role  
-        this.jobs = user.jobs 
-        this.job_applications = user.job_applications
-        //this.applied_jobs = user.applied_jobs
-        //console.log(this.applied_jobs)
-                 
-    }
+  constructor(user) {
+    this.id = user.id 
+    this.name = user.name 
+    this.role = user.role  
+    this.jobs = user.jobs 
+    this.job_applications = user.job_applications
+    //this.applied_jobs = user.applied_jobs
+    //console.log(this.applied_jobs)
+  }
 }
 
 
 User.prototype.formatShow = function() {  
-    //applicant not working. Can't access app.job.title****
-    let applicantHtml = this.job_applications.map(application => { 
-        //console.log(this.applied_jobs)        
-        return (`
-        <li><a href='/job_applications/${application.id}' data-id="${application.id}">${application.id}</a> | Job Title | Company Name | Applied: ${new Date(application.created_at).toDateString()} </li>
-        `)
-    }).join('')
-    
-    let companyHtml = this.jobs.map(job => {
-        return (`
-        <li><a href='/jobs/${job.id}' data-id="${job.id}" class="show_link">${job.title}</a> | ${job.location} | Date Posted: ${new Date(job.created_at).toDateString()}</li>
+  //applicant not working. Can't access app.job.title****
+  let applicantHtml = this.job_applications.map(application => { 
+    //console.log(this.applied_jobs)        
+    return (`
+      <li><a href='/job_applications/${application.id}' data-id="${application.id}">${application.id}</a> | Job Title | Company Name | Applied: ${new Date(application.created_at).toDateString()} </li>
     `)
-    }).join('')
-    if(this.role === 'company')
+  }).join('')
+    
+  let companyHtml = this.jobs.map(job => {
+    return (`
+    <li><a href='/jobs/${job.id}' data-id="${job.id}" class="show_link">${job.title}</a> | ${job.location} | Date Posted: ${new Date(job.created_at).toDateString()}</li>
+    `)
+  }).join('')
+  if(this.role === 'company')
     return (` 
     <h3>Hi ${this.name}</h3>
     <h4>Type: ${this.role}</h4>
@@ -136,7 +132,7 @@ User.prototype.formatShow = function() {
     <br>
     <ul>${companyHtml}</ul>
     `) 
-    else     
+  else     
     return (`
     <h3>Hi ${this.name}</h3>
     <h4>Type: ${this.role}</h4>
@@ -145,25 +141,25 @@ User.prototype.formatShow = function() {
     <h4>Jobs Applied To</h4>
     <br>
     <ul>${applicantHtml}</ul> 
-    `)
+  `)
 }
 
 
 User.prototype.formatLinks = function() {
   if(this.role === 'company')
-  return (`
-  <br>
-  <br>
-  <a href="/jobs/new" class='new_job_form'>Post New Job</a> |
-  <a href="/jobs" class='all_jobs'>All Jobs</a> |  
-  <a href="/signout" class='sign_out'>Log Out</a>
-  `) 
+    return (`
+    <br>
+    <br>
+    <a href="/jobs/new" class='new_job_form'>Post New Job</a> |
+    <a href="/jobs" class='all_jobs'>All Jobs</a> |  
+    <a href="/signout" class='sign_out'>Log Out</a>
+    `) 
   else 
-  return (`
-  <br>
-  <br>
-  <a href="/jobs" class='all_jobs'>All Jobs</a> |  
-  <a href="/signout" class='sign_out'>Log Out</a>
+    return (`
+    <br>
+    <br>
+    <a href="/jobs" class='all_jobs'>All Jobs</a> |  
+    <a href="/signout" class='sign_out'>Log Out</a>
   `)
 }
 
